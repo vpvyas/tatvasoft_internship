3f258ecef78cde3f47aff0c02069dc6f0c6ad8ac
vishva vyas <vpvyas03@gmail.com>
2025-07-14 23:27:08 +0530

    Add files via upload

---
+++ day3/ILoginRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mission.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Repositories.IRepositories
+{
+    public interface ILoginRepository
+    {
+        User login(string EmailAddress, string Password);
+    }
+}
---
+++ day3/LoginRepository.cs
@@ -0,0 +1,24 @@
+﻿using Mission.Entities.context;
+using Mission.Entities.Entities;
+using Mission.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Repositories.Repositories
+{
+    public class LoginRepository(MissionDbContext missionDbContext): ILoginRepository
+    {
+        private readonly MissionDbContext _missionDbContext = missionDbContext;
+        public User login(string EmailAddress , string Password)
+        {
+            var user = _missionDbContext.Users.Where(x => x.EmailAddress == EmailAddress && x.Password == Password).FirstOrDefault();
+            if (user == null) {
+                return null;
+            }
+            return user;
+        }
+    }
+}
