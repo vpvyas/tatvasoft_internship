013ed350862b421f8da8bb619087f9123e452315
vishva vyas <vpvyas03@gmail.com>
2025-07-15 10:30:02 +0530

    Add files via upload

---
+++ day7/Mission.Entities/context/MissionDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Entities;
+
+namespace Mission.Entities.context
+{
+    public class MissionDbContext : DbContext
+    {
+        public MissionDbContext(DbContextOptions<MissionDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<MissionTheme> MissionTheme { get; set; }
+        public DbSet<MissionSkill> MissionSkill { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Missions> Missions { get; set; }   
+        public DbSet<MissionApplication> MissionApplications { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegancyTimestampBehavior", true);
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Tatva",
+                LastName = "Admin",
+                EmailAddress = "admin@tatvasoft.com",
+                UserType = "admin",
+                Password = "Tatva@123",
+                PhoneNumber = "9876543210",
+                CreatedDate = new DateTime(2019, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+            });
+
+            modelBuilder.Entity<Country>().HasData(new Country()
+            {
+               Id = 1,
+               CountryName = "India",
+            });
+
+            modelBuilder.Entity<City>().HasData(new City()
+            {
+                Id = 1,
+                CityName = "Ahmedabad",
+                CountryId = 1
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
