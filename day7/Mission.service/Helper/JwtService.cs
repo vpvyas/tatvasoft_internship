015b0fbf043a63cb8efb82efe17c99090105ab4b
vishva vyas <vpvyas03@gmail.com>
2025-07-15 10:15:09 +0530

    Add files via upload

---
+++ day7/Mission.service/Helper/JwtService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.Helpers
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetToken(string userId, string name, string email, string role)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new Claim[]
+            {
+new Claim("firstName",name),
+                new Claim("emailAddress",email),
+                new Claim("userType",role),
+                new Claim("userId", userId)
+            };
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
